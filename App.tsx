a95df0de189de063703a078d6ec8515c7eb16517
abidine <bakari06@live.fr>
2025-10-13 00:21:30 +0200

    chore: configuration App.tsx principal

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MapScreen from './src/screens/MapScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
+      <MapScreen />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
