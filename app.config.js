ac71cfbbbc729b945efb658b30267caf19d0b0de
abidine <bakari06@live.fr>
2025-11-04 09:41:23 +0100

    fix: add custom config plugin to inject Google Maps API key in AndroidManifest

---
+++ app.config.js
@@ -0,0 +1,39 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+const appJson = require('./app.json');
+
+const withGoogleMapsApiKey = (config, apiKey) => {
+  return withAndroidManifest(config, (config) => {
+    const androidManifest = config.modResults;
+    const application = androidManifest.manifest.application[0];
+
+    // Ajouter la méta-donnée com.google.android.geo.API_KEY
+    if (!application['meta-data']) {
+      application['meta-data'] = [];
+    }
+
+    // Supprimer toute clé existante
+    application['meta-data'] = application['meta-data'].filter(
+      (item) => item.$['android:name'] !== 'com.google.android.geo.API_KEY'
+    );
+
+    // Ajouter la nouvelle clé
+    application['meta-data'].push({
+      $: {
+        'android:name': 'com.google.android.geo.API_KEY',
+        'android:value': apiKey,
+      },
+    });
+
+    return config;
+  });
+};
+
+module.exports = ({ config }) => {
+  // Fusionner avec app.json
+  const baseConfig = {
+    ...appJson.expo,
+    ...config,
+  };
+
+  return withGoogleMapsApiKey(baseConfig, 'AIzaSyDuQzziyNDt7gNPEOMEmaqhPr0RaVTuNK8');
+};
